539464a735b25e387b3d72150eeeba5a4c08eaae
Jefferson Doljak <jeffersondoljak@MacBook-Pro.local>
2024-02-05 22:58:45 -0300

    init commit / material design - install & custom css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppMaterialModule } from './shared/app-material/app-material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppMaterialModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'crud-angular';
---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [MatToolbarModule],
+})
+export class AppMaterialModule {}
